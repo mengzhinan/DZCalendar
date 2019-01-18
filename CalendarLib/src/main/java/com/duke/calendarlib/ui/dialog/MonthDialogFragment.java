8dd62703e20c82acd4287df8f58114495361bc46
duke <136239173@qq.com>
2019-01-18 21:45:01 +0800

    d

---
+++ CalendarLib/src/main/java/com/duke/calendarlib/ui/dialog/MonthDialogFragment.java
@@ -0,0 +1,12 @@
+package com.duke.calendarlib.ui.dialog;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * @ Author: duke
+ * @ DateTime: 2019-01-18 21:44
+ * @ Description:
+ */
+public class MonthDialogFragment extends DialogFragment {
+
+}
